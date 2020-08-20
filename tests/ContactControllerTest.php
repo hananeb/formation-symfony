54c753d3b4ed4b115f4c931a97f07dec66c55e8a
Hanane BOUHADA <hanane.bouhada@smile-maroc.com>
2020-08-20 08:37:21 +0100

    Test Contact

--- tests/ContactControllerTest.php
+++ tests/ContactControllerTest.php
@@ -9,10 +9,35 @@ class ContactControllerTest extends WebTestCase
     public function testWeCanContact()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/contact');
+        $client->request('GET', '/contact');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists("#contact_email");
         $this->assertSelectorExists("#contact_message");
+        $client->submitForm('Contacter', [
+            'contact[email]' => 'test@smile.fr',
+            'contact[message]' => 'test',
+              ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+
+        $this->assertSelectorTextContains('div.alert-success', 'Merci d\'avoir pris contact');
     }
+
+    /**
+     * Sad path / Unhappy path
+     */
+    public function testTheContactFormDisplaysErrors()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/contact');
+
+        $email = $crawler->filter('#contact_email');
+        $message = $crawler->filter('#contact_message');
+
+        $this->assertCount(1, $email);
+        $this->assertCount(1, $message);
+    }
+
 }
